742c7d4b57b92ebba8d3748b259668af8ccb5021
Christian Cuenca <43630446+Chrisfred@users.noreply.github.com>
2019-04-24 12:44:02 -0500

    Se actualizo con Sublime
    Se agregó una operación matematica

--- Hola Mundo.py
+++ Hola Mundo.py
@@ -1,5 +1,21 @@
-Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> print ("hola")
-hola
->>> 
+print ("hola mundo")
+
+a=2
+b=2
+c=a*b
+print (c)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
